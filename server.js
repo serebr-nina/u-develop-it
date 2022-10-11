b73fb37966a603fecb0a2a937f2b9f71d9f2af09
NinaSerebryakova <serebr.nina@gmail.com>
2022-10-10 21:51:59 -0400

    created queries for read, delete, create operations

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mysql = require('mysql2');
- 
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -18,10 +18,42 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the election database.')
 );
-
+/*
 db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
 });
+*/
+/*
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+*/
+/*
+// Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+*/
+/*
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+*/
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
